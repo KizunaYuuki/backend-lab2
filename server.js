a71e0365dc620cac5ba30a05c2b58a3308948b92
KizunaYuuki <time0009000@gmail.com>
2022-09-23 09:35:31 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
 
-// start server
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});
+async function startServer() {
+    try {
+        await MongoDB.connect(config.db.uri); // get uri
+        console.log("Connected to the database!"); // notice connected
+
+        const PORT = config.app.port; // get port to /app/config/index.js
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log("Cannot connect to the database!", error);
+        process.exit();
+    }
+}
+
+startServer();
